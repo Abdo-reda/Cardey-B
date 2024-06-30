cf3b8c8812ad80aa7fb7db155096cb131e3b6b09
Abdo-reda <abdo.reda@aucegypt.edu>
2024-06-30 22:15:31 +0300

    a7san component nour shafoh fe 7yatoh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { Avatar, Button, FormItem, Input } from 'ant-design-vue';
-import { computed, ref } from 'vue';
+import { Avatar, Button, Form, FormItem, Input } from 'ant-design-vue';
+import { reactive, ref } from 'vue';
 
 const name = ref('');
 
@@ -8,6 +8,22 @@ const name = ref('');
 //     name.value.length > 56
 //     return name.value;
 // });
+
+interface FormState {
+    name: string;
+}
+
+const joinFormState = reactive<FormState>({
+    name: '',
+});
+
+function onSumbit() {
+    console.log("I am submitttingggg!!!!")
+}
+
+function onFail() {
+    console.log("I am failinggggg!!!!")
+}
 
 </script>
 
@@ -17,19 +33,26 @@ const name = ref('');
             <p class="font-semibold text-3xl"> Cardey-B Game </p>
         </div>
         <div class="flex flex-col h-5/6 bg-red-200 items-center justify-center m-4 p-4">
-            <div class="flex flex-col justify-center items-center">
-                <Avatar class="hover:cursor-pointer size-32">
-                </Avatar>
-                <div class="flex flex-row items-center justify-center m-4 gap-4">
-                    <FormItem class="m-0">
-                        <Input v-model:value="name" />
-                    </FormItem>
+            <Form :model="joinFormState" @finish="onSumbit" @finish-failed="onFail">
+                <div class="flex flex-col justify-center items-center">
+                    <Avatar class="hover:cursor-pointer size-32">
+                    </Avatar>
+                    <div class="flex flex-row items-center justify-center m-4 gap-4">
+                        <FormItem class="m-0" name="name"
+                            :rules="[{ required: true, message: 'Please input your name' }]">
+                            <Input placeholder="name" v-model:value="joinFormState.name" />
+                        </FormItem>
+                    </div>
+                    <div class="flex items-center justify-center gap-4">
+                        <FormItem>
+                            <Button type="primary" html-type="submit"> Host Game </Button>
+                        </FormItem>
+                        <FormItem>
+                            <Button type="dashed" html-type="submit"> Join Game </Button>
+                        </FormItem>
+                    </div>
                 </div>
-                <div class="flex items-center justify-center gap-4">
-                    <Button type="primary"> Host Game </Button>
-                    <Button type="dashed"> Join Game </Button>
-                </div>
-            </div>
+            </Form>
         </div>
     </div>
 </template>
