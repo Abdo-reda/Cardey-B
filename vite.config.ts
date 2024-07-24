0658fa77965101095597990053f8170985456c70
Abdo-reda <abdo.reda@aucegypt.edu>
2024-07-24 17:13:43 +0300

    init plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,18 @@ import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import svgLoader from 'vite-svg-loader';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			}
+		}),
 		svgLoader({
 			defaultImport: 'component'
 		}),
@@ -26,3 +33,30 @@ export default defineConfig({
 		}
 	}
 });
+
+// injectRegister: false,
+
+// pwaAssets: {
+//   disabled: false,
+//   config: true,
+// },
+
+// manifest: {
+//   name: 'vue-pwa-vite',
+//   short_name: 'vue-pwa-vite',
+//   description: 'This is a description for pwa',
+//   theme_color: '#1f1f1f',
+// },
+
+// workbox: {
+//   globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+//   cleanupOutdatedCaches: true,
+//   clientsClaim: true,
+// },
+
+// devOptions: {
+//   enabled: false,
+//   navigateFallback: 'index.html',
+//   suppressWarnings: true,
+//   type: 'module',
+// },
