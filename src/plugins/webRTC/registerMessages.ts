832da89f516ac17c24ae450e3e76fefd236c17e9
Abdo-reda <abdo.reda@aucegypt.edu>
2024-08-06 23:53:53 +0300

    removed unused logic

--- src/plugins/webRTC/registerMessages.ts
+++ src/plugins/webRTC/registerMessages.ts
@@ -3,7 +3,7 @@ import { JoinTeamMessage } from '@/core/models/messages/joinTeamMessage';
 import { PlayerWordsMessage } from '@/core/models/messages/playerWordsMessage';
 import { SyncMessage } from '@/core/models/messages/syncMessage';
 
-export default function registerMessages() {
+export default function () {
 	new JoinGameMessage();
 	new JoinTeamMessage();
 	new SyncMessage();
