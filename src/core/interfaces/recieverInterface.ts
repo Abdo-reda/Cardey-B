23086b38a841c21c0fb0fe50e2d06ae286af229f
Abdo-reda <abdo.reda@aucegypt.edu>
2024-08-10 16:46:16 +0300

    removed unused code

--- src/core/interfaces/recieverInterface.ts
+++
@@ -1,7 +0,0 @@
-import type { Ref } from 'vue';
-import type { IGameState } from './gameStateInterface';
-import type { IMessage } from './messageInterfaces/messageInterface';
-
-export interface IReciever<T> {
-	handle: (gameState: Ref<IGameState>, message: IMessage<T>) => void;
-}
