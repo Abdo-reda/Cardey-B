d1dab4d93cc63942c587be449431833cf3690435
Abdo-reda <abdo.reda@aucegypt.edu>
2025-04-12 10:54:39 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,63 +1,14 @@
-# cardey-b
+# Cardey-B
 
-This template should help get you started developing with Vue 3 in Vite.
+![Logo](./public/pwa-192x192.png)
 
-## Recommended IDE Setup
+**A Collection of Online Party Card Games!**
 
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+Currently there is only one game though ... ¯\_(ツ)\_/¯
 
-## Type Support for `.vue` Imports in TS
+## Screenshots
 
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
-## Project Setup
-
-```sh
-npm install
-```
-
-### Compile and Hot-Reload for Development
-
-```sh
-npm run dev
-```
-
-### Type-Check, Compile and Minify for Production
-
-```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
-```
-
-### Notes
-
--   disable the start until all players have joined team.
--   player can only join, if the current screen/phase is Lobby [currentRoute]
--   I am host, I want to send message, to tell players go to next screen "X screen" (go to hell), I want to tell players go to (phase/screen/) "begin-game"
-    -   New message RouteTo(RouteName)
-    -   New reciever
--   Each player has two messages, AddAllWords, RemoveAllWords -- (playerId) maybe the words are stored as a dictionary? <id, words[]>
-    -   we can check the number of keys in the dictionary to know when players are ready.
-    -   Maybe add a view to know we are waiting for which player.
--   Keep track of game phase.
-    -   we have 3 phases, each phase a description. (just a name)
-    -   when we go to a new phase, we shuffle the words and start from the beggining.
-    -   but we keep track which player/team had the turn.
--   Keep track of:
-    -   skipped words.
-    -   passed words.
-    -   remaining words.
-    -   current team, and current player.
--   Once there are no more remaining words, we go the next game phase.
+![mobile_screenshot_1](./public/mobile_screenshot_1.png)
 
 ### Enhancements
 
@@ -68,8 +19,7 @@ npm run lint
 -   [ ] Add CaptCha to secure connection to firebase
 -   [ ] Adding a PageHeader might be nice/needed for supporting PWAs
 -   [ ] Having a notification / message / alert when getting disconnected?
--   [ ]
 
 ### Bugs / Warnings
 
--   [ ] v-auto-animate in the lobby, probably because I have two nested v-auto-aniamte.
+-   [ ] v-auto-animate in the lobby, probably because I have two nested v-auto-animate.
--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@ import {
 	Modal,
 	PageHeader,
 	Result,
-  TypographyTitle,
+	TypographyTitle,
 } from 'ant-design-vue';
 import { RouterView } from 'vue-router';
 import {
 	SettingOutlined,
 	FormatPainterOutlined,
 	PauseCircleFilled,
-  InfoCircleOutlined,
+	InfoCircleOutlined,
 	PlayCircleFilled,
 	ArrowLeftOutlined
 } from '@ant-design/icons-vue';
@@ -27,7 +27,7 @@ import ChatComponent from './components/Layout/ChatComponent.vue';
 import SettingModalComponent from './components/Modals/SettingModalComponent.vue';
 import InfoModalComponent from './components/Modals/InfoModalComponent.vue';
 
-const version =  __APP_VERSION__;
+const version = __APP_VERSION__;
 const gameService = inject(GameServiceKey)!;
 const { currentPlayer } = usePlayer();
 const settingsOpen = ref(false);
@@ -63,22 +63,16 @@ function handleGoBack() {
 </script>
 
 <template>
-	<ConfigProvider
-		:theme="{
-			algorithm: currentThemeAlgorithm
-		}"
-	>
+	<ConfigProvider :theme="{
+		algorithm: currentThemeAlgorithm
+	}">
 		<main class="h-screen max-h-screen flex flex-col p-6">
 			<div>
 				<PageHeader @back="handleGoBack()">
 					<template #backIcon>
-						<Button
-							v-if="router.currentRoute.value.name !== RoutesEnum.HOME"
-							size="large"
+						<Button v-if="router.currentRoute.value.name !== RoutesEnum.HOME" size="large"
 							class="flex flex-col justify-center items-center text-gray-400 dark:text-gray-300"
-							type="default"
-							shape="circle"
-						>
+							type="default" shape="circle">
 							<template #icon>
 								<ArrowLeftOutlined />
 							</template>
@@ -86,36 +80,23 @@ function handleGoBack() {
 					</template>
 					<template #extra>
 						<div v-auto-animate class="flex gap-x-2">
-							<Button
-								@click="switchTheme"
-								size="large"
+							<Button @click="switchTheme" size="large"
 								class="flex flex-col justify-center items-center text-gray-400 dark:text-gray-300"
-								type="default"
-								shape="circle"
-							>
+								type="default" shape="circle">
 								<template #icon>
 									<FormatPainterOutlined />
 								</template>
 							</Button>
-							<Button
-								@click="settingsOpen = true"
-								size="large"
+							<Button @click="settingsOpen = true" size="large"
 								class="flex flex-col justify-center items-center text-gray-400 dark:text-gray-300"
-								type="default"
-								shape="circle"
-							>
+								type="default" shape="circle">
 								<template #icon>
 									<SettingOutlined />
 								</template>
 							</Button>
-							<Button
-								v-if="currentPlayer.isHost"
-								@click="togglePause"
-								size="large"
+							<Button v-if="currentPlayer.isHost" @click="togglePause" size="large"
 								class="flex flex-col justify-center items-center text-gray-400 dark:text-gray-300"
-								type="default"
-								shape="circle"
-							>
+								type="default" shape="circle">
 								<template #icon>
 									<PlayCircleFilled v-if="isPaused" />
 									<PauseCircleFilled v-else />
@@ -127,50 +108,35 @@ function handleGoBack() {
 			</div>
 			<RouterView v-slot="{ Component, route }">
 				<Transition name="fade" mode="out-in">
-					<component
-						:key="route.name"
-						class="flex-1 max-h-full overflow-hidden"
-						:is="Component"
-					/>
+					<component :key="route.name" class="flex-1 max-h-full overflow-hidden" :is="Component" />
 				</Transition>
 			</RouterView>
-      <div class="mx-6 my-8 w-fit">
-        <Button @click="infoOpen=true" type="text" class="flex items-center" size="large"> 
-          <InfoCircleOutlined />
-          V {{version}}
-        </Button>
-        <!-- <TypographyTitle class="!m-0" :level="4">
+			<div class="mx-6 my-8 w-fit">
+				<Button @click="infoOpen = true" type="text" class="flex items-center" size="large">
+					<InfoCircleOutlined />
+					V {{ version }}
+				</Button>
+				<!-- <TypographyTitle class="!m-0" :level="4">
        
          
         </TypographyTitle> -->
-        <InfoModalComponent  v-model="infoOpen" />
-      </div>
+				<InfoModalComponent v-model="infoOpen" />
+			</div>
 		</main>
 		<!-- CHAT -->
 		<ChatComponent />
-    <!-- Setting Modal -->
-    <SettingModalComponent v-model="settingsOpen" />
+		<!-- Setting Modal -->
+		<SettingModalComponent v-model="settingsOpen" />
 		<!-- Pause Modal -->
-		<Modal
-			:centered="true"
-			:keyboard="false"
-			:maskClosable="false"
-			v-model:open="isPaused"
-			:closable="false"
-		>
+		<Modal :centered="true" :keyboard="false" :maskClosable="false" v-model:open="isPaused" :closable="false">
 			<Result sub-title="only the host can unpause">
 				<template #title>
 					<p class="font-semibold">The Game is Paused!</p>
 				</template>
 				<template #extra>
 					<div class="flex items-center justify-center">
-						<Button
-							v-if="currentPlayer.isHost"
-							@click="togglePause"
-							class="flex items-center"
-							key="play"
-							type="primary"
-						>
+						<Button v-if="currentPlayer.isHost" @click="togglePause" class="flex items-center" key="play"
+							type="primary">
 							<template #icon>
 								<PlayCircleFilled />
 							</template>
@@ -182,14 +148,8 @@ function handleGoBack() {
 			<template #footer> </template>
 		</Modal>
 		<!-- Quit Game Confirmation Modal -->
-		<Modal
-			@ok="quitGame()"
-			:centered="true"
-			:keyboard="true"
-			:maskClosable="true"
-			v-model:open="quitModalOpen"
-			:closable="true"
-		>
+		<Modal @ok="quitGame()" :centered="true" :keyboard="true" :maskClosable="true" v-model:open="quitModalOpen"
+			:closable="true">
 			<Result status="warning">
 				<template #title>
 					<p class="font-semibold">Are you sure you want to quit ?</p>
